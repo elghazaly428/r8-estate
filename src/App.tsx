2ea293a5a7be9ad05c4d0d419287984069ed7182
elghazaly428 <mahmoud4228@yahoo.com>
2025-06-17 16:03:53 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,13 +31,13 @@ function App() {
 
   const text = {
     ar: {
-      heroTitle: 'مهمتنا: تعزيز الشفافية للقطاع العقاري',
+      heroTitle: 'منصة تقييم القطاع العقاري الأولي في مصر',
       heroSubtitle: 'اكتشف وقيم أفضل العقارات والمطورين في مصر. تقييمات حقيقية من عملاء حقيقين',
       ctaQuestion: 'لسا طالع من تجربة مع شركة ؟',
       ctaAction: 'شاركها معانا'
     },
     en: {
-      heroTitle: 'Our Mission: Enhancing Transparency for the Real Estate Sector',
+      heroTitle: 'The Primary Real Estate Sector Review Platform in Egypt',
       heroSubtitle: 'Discover and review the best real estate and developers in Egypt. Real reviews from real customers',
       ctaQuestion: 'Just had an experience with a company?',
       ctaAction: 'Share it with us'
